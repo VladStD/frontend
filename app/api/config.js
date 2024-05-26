7322086975ab8763bed44d88b4105a36946020f0
Vladst <149588172+VladStD@users.noreply.github.com>
2024-05-26 21:55:12 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://vladst-frontend.nomoredomainswork.ru/api'
+export const BASE_URL = 'http://vladst-backend.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
