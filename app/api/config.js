7d45189d4d745bf86ab6ebea76246b5476390777
VladStD <vladstcloud@mail.ru>
2024-05-26 22:15:09 +0300

    BBBBB

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://vladst-frontend.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://vladst-backend.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
